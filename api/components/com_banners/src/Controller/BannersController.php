0b7db5491587121560f6d58933de8fe6ec56bf40
Hannes Papenberg <info@joomlager.de>
2025-08-08 12:55:38 +0200

    More changes 3

--- api/components/com_banners/src/Controller/BannersController.php
+++ api/components/com_banners/src/Controller/BannersController.php
@@ -21,7 +21,8 @@ use Joomla\CMS\MVC\Controller\ApiController;
  *
  * @since  4.0.0
  */
-class BannersController extends ApiController{
+class BannersController extends ApiController
+                                           {
     /**
      * The content type of the item.
      *
