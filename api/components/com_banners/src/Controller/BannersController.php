2b5866b98a50c120365686d206ffce7e9d0b1a5a
Hannes Papenberg <info@joomlager.de>
2026-02-12 09:45:47 +0100

    Update BannersController.php

--- api/components/com_banners/src/Controller/BannersController.php
+++ api/components/com_banners/src/Controller/BannersController.php
@@ -21,7 +21,7 @@ use Joomla\CMS\MVC\Controller\ApiController;
  *
  * @since  4.0.0
  */
-class BannersController extends ApiController                                          {
+class BannersController extends ApiController                                         {
     /**
      * The content type of the item.
      *
